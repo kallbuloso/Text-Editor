056d56806beee777a0f068c1d948ebfdcf45c3d1
Brian Savatia Keyonzo <briankeyonzo@yahoo.com>
2015-12-07 02:19:53 +0800

    Finished Text Editor

---
+++ DocumentManager.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace TextEditor
+{
+    class DocumentManager
+    {
+        private string _currentFile;
+        private RichTextBox _textBox;
+
+        public DocumentManager(RichTextBox textBox)
+        {
+            _textBox = textBox;
+        }
+
+        public bool OpenDocument()
+        {
+            OpenFileDialog dig = new OpenFileDialog();
+            dig.Filter = "All Text Documents (.rtf, .txt)|*.rtf; *.txt|Text Documents (.rtf)|*.rtf|Text Files (.txt)|*.txt|All Files (*.*)|*.*";
+
+            if (dig.ShowDialog() == true)
+            {
+                this._currentFile = dig.FileName;
+
+                using(Stream stream = dig.OpenFile())
+                {
+                    TextRange range = new TextRange( _textBox.Document.ContentStart, _textBox.Document.ContentEnd);
+                    range.Load(stream, DataFormats.Rtf);
+                }
+                return true;
+            }
+
+            return false;
+        }
+
+        public void NewDocument()
+        {
+  
+            _currentFile = null;
+            _textBox.Document = new FlowDocument();
+        }
+
+        public bool SaveDocument()
+        {
+            if (string.IsNullOrEmpty(_currentFile)) return SaveDocumentAs();
+            else
+            {
+                using(Stream stream = new FileStream(_currentFile, FileMode.Create))
+                {
+                    TextRange range = new TextRange(_textBox.Document.ContentStart, _textBox.Document.ContentEnd);
+                    range.Save(stream, DataFormats.Rtf);
+                }
+                return true;
+            }
+        }
+
+        public bool SaveDocumentAs()
+        {
+            SaveFileDialog dig = new SaveFileDialog();
+
+            if(dig.ShowDialog() == true)
+            {
+                _currentFile = dig.FileName;
+                return SaveDocument();
+            }
+
+            return false;
+        }
+
+        public void ApplyToSelection(DependencyProperty property, object value)
+        {
+            if (value != null) _textBox.Selection.ApplyPropertyValue(property, value);
+        }
+
+        public bool CanSaveDocument()
+        {
+            return !string.IsNullOrEmpty(_currentFile);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DocumentManager _documentManager;
+        private PrintManager _printManager;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _documentManager = new DocumentManager(body);
+            _printManager = new PrintManager(body);
+            if (_documentManager.OpenDocument()) status.Text = "Document loaded.";
+        }
+
+        private void toolbar_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            if (toolbar.IsSynchronizing) return;
+
+            ComboBox source = (ComboBox)e.OriginalSource;
+
+            if (source == null) return;
+
+            switch (source.Name)
+            {
+                case "fonts":
+                    _documentManager.ApplyToSelection(TextBlock.FontFamilyProperty, source.SelectedItem);
+                    break;
+                case "fontSize":
+                    _documentManager.ApplyToSelection(TextBlock.FontSizeProperty,  source.SelectedItem);
+                    break;
+            }
+
+            body.Focus();
+        }
+
+        private void body_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            toolbar.SynchronizeWith(body.Selection);
+        }
+
+        private void NewDocument(object sender, ExecutedRoutedEventArgs e)
+        {
+            _documentManager.NewDocument();
+            status.Text = "New Document";
+        }
+
+        private void OpenDocument(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (_documentManager.OpenDocument()) status.Text = "Document loaded.";
+        }
+
+        private void SaveDocument(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (_documentManager.SaveDocument()) status.Text = "Document saved.";
+        }
+
+        private void SaveDocumentAs(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (_documentManager.SaveDocumentAs()) status.Text = "Document saved.";
+        }
+
+        private void ApplicationClose(object sender, ExecutedRoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void SaveDocument_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = _documentManager.CanSaveDocument();
+        }
+
+        private void PrintDocument(object sender, ExecutedRoutedEventArgs e)
+        {
+            PrintDialog dlg = new PrintDialog();
+
+            if (dlg.ShowDialog() == true)
+            {
+                dlg.PrintDocument(((IDocumentPaginatorSource)body.Document).DocumentPaginator,"Text Editor Printing");
+            }
+        }
+
+        private void PrintPreview(object sender, ExecutedRoutedEventArgs e)
+        {
+            _printManager.PrintPreview();
+        }
+    }
+}
